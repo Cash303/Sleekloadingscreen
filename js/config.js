28743758dd59f53b8646248cb1d9a7795c17c12d
Cash303 <cashwoolsey@gmail.com>
2017-10-03 19:22:13 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -38,6 +38,8 @@ Avaiable options:
 You can add custom backgrounds
 Just make sure it is at webm VP8 format */
 var l_background = [ "backgrounds/videos/bluefall.webm"
+ 
+];
 
 /*(Image-background only)
 Place one or more images
